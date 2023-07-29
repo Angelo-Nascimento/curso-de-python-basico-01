b7d5e7e40917b27e48bd7e71e561d5041def819e
__Angelo__ <85811107+Angelo-Nascimento@users.noreply.github.com>
2023-07-28 22:32:41 -0300

    Create First script in python.py

---
+++ Aula 04/First script in python.py
@@ -0,0 +1,8 @@
+print('What is your name?')
+name = input('Type here: ')
+print('How old are you?')
+age = input('Type here: ')
+print('What is your weight?')
+weight = input('Type here: ')
+
+print('My name is : ', name, ', i am ', age, ' old years and i am weighing ', weight, ' kilos')
