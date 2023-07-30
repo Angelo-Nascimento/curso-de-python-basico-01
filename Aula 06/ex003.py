bc01107cec888f77254147f56cfe5c74c3a0e09f
__Angelo__ <85811107+Angelo-Nascimento@users.noreply.github.com>
2023-07-30 18:02:23 -0300

    Create ex003.py

---
+++ Aula 06/ex003.py
@@ -0,0 +1,8 @@
+# Desafio oo3
+# Crie um programa que leia dois números e mostre a soma entre eles.
+print("Program to sum two ages")
+age01 = int(input("Enter the first age here: "))
+age02 = int(input("Enter the second age here: "))
+sum = age01 + age02
+
+print("The sum of the first age: {} + of second age: {} = {}".format(age01, age02, sum))
