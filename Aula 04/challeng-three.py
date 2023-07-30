f96ace5ad3399a0695e79e31c6f57b1b954549f9
__Angelo__ <85811107+Angelo-Nascimento@users.noreply.github.com>
2023-07-29 21:10:44 -0300

    Create challeng-three.py

---
+++ Aula 04/challeng-three.py
@@ -0,0 +1,5 @@
+print('========== CHALLENG 03 =========')
+numberOne = input('Type first number: ')
+numberTwo = input('Type second number: ')
+
+print('A soma dos números é: ', numberOne + numberTwo)
