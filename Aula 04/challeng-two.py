ef07ac67eba4b3e2d8f2fd8c8ff67a090b158820
__Angelo__ <85811107+Angelo-Nascimento@users.noreply.github.com>
2023-07-29 21:10:18 -0300

    Create challeng-two.py

---
+++ Aula 04/challeng-two.py
@@ -0,0 +1,7 @@
+print('========= CHALLENG 02 ======')
+print('Program to show your age!')
+day = input('Day = ')
+month = input('Month = ')
+year = input('Year = ')
+
+print('You were born on ', month, day, ' , ', year, ', correct?')
