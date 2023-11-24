ecdec4698c4db3d32c161827b3596efba046d50d
__Angelo__ <85811107+Angelo-Nascimento@users.noreply.github.com>
2023-11-23 23:01:38 -0300

    Create ex005.py

---
+++ Aula 07/ex005.py
@@ -0,0 +1,5 @@
+# Desafio 05
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucesso e seu antecessor
+
+num = int(input('Digite um número: '))
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}' .format(num, (num-1), (num+1)))
